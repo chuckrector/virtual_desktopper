e8ae5cf378c0c442ca08c587635dd4fb1ad85b97
Chuck Rector <choosetheforce@gmail.com>
2021-02-27 19:34:58 -0800

    Slightly more generic env

--- s/shell.bat
+++ s/shell.bat
@@ -5,7 +5,7 @@ set path=C:\remedy;C:\dev\virtual_desktopper\s;C:\dev\virtual_desktopper\build;%
 rem vcvarsall.bat won't configure things properly (?) without this.
 rem If system32 is not included, stdio.h cannot be found when running build.bat!
 rem C:\Windows doesn't seem necessary for building, but might as well.
-set path=%path%;C:\WINDOWS\system32;C:\WINDOWS;C:\Users\Charles\AppData\Local\Programs\Microsoft VS Code\bin;C:\Program Files\LLVM\bin
+set path=%path%;C:\WINDOWS\system32;C:\WINDOWS;%userprofile%\AppData\Local\Programs\Microsoft VS Code\bin;C:\Program Files\LLVM\bin
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 start remedybg c:\dev\virtual_desktopper\virtual_desktopper.rdbg
